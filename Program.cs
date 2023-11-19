f7cac698d5d5b1567c344042097462a0006cf2e8
Jakub Perkowski <kubaperkowski22@gmail.com>
2023-11-19 18:53:18 +0100

    Add new plan - finished

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Text;
 
 bool IsProgramRunning = true;
 List<Plan> PlanList = new List<Plan>();
+string VisibleText = "";
 
 while (IsProgramRunning)
 {
@@ -78,31 +79,146 @@ void ShowMainOptions(int option, string decorator)
 void AddNewPlan()
 {
     Console.Clear();
+
+    VisibleText += "Tworzysz nowy plan\n\n";
     Console.WriteLine("Tworzysz nowy plan\n");
+
     Plan plan = CreateNewPlan();
     PlanList.Add(plan);
-    Console.ReadKey();
 }
 Plan CreateNewPlan()
 {
+    VisibleText += "Podaj nazwę planu: \n";
     Console.WriteLine("Podaj nazwę planu: ");
+
     string name = Console.ReadLine();
+    VisibleText += name + '\n';
     Console.WriteLine("Podaj datę końca wydarzenia(RRRR-MM-DD): ");
-    string dateTime = Console.ReadLine();
+    VisibleText += "Podaj datę końca wydarzenia(RRRR-MM-DD): \n";
+    DateTime dateTime;
+
+    bool isDateTimeFormatCorrect = false;
+    while (!isDateTimeFormatCorrect)
+    {
+        string dateTimeString = Console.ReadLine();
+        if (!CheckDateTimeFormat(dateTimeString))
+        {
+            Console.Clear();
+            Console.WriteLine(VisibleText);
+            continue;
+        }
+        dateTime = DateTime.Parse(dateTimeString);
+
+        if (dateTime < DateTime.Now)
+        {
+            Console.Clear();
+            Console.WriteLine("BŁĄD! Data wydarzenia nie może być wcześniejsza niż aktualna data!");
+            System.Threading.Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine(VisibleText);
+        }
+        else
+        {
+            VisibleText += dateTime;
+            VisibleText += '\n';
+            isDateTimeFormatCorrect = true;
+        }
+    }
+
     Console.WriteLine("Podaj priorytet (1-10): ");
+    VisibleText += "Podaj priorytet (1-10): \n";
     int priority = Convert.ToInt32(Console.ReadLine());
+
+    bool isPriorityCorrect = false;
+    while (!isPriorityCorrect)
+    {
+        if (CheckPriority(priority))
+        {
+            VisibleText += priority + '\n';
+            isPriorityCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine(VisibleText);
+            priority = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    
     Console.WriteLine("Podaj kategorię: ");
     string category = ChooseCategory();
+    System.Threading.Thread.Sleep(1000);
 
-    Console.WriteLine("Wybrałeś kategorię:" + category);
+    Console.WriteLine("Pomyslnie stworzono nowy plan.\n\n");
+    Console.WriteLine(".d8888b 888  888 .d8888b .d8888b .d88b. .d8888b .d8888b  \r\n88K     888  888d88P\"   d88P\"   d8P  Y8b88K     88K      \r\n\"Y8888b.888  888888     888     88888888\"Y8888b.\"Y8888b. \r\n     X88Y88b 888Y88b.   Y88b.   Y8b.         X88     X88 \r\n 88888P' \"Y88888 \"Y8888P \"Y8888P \"Y8888  88888P' 88888P' ");
 
-    //Console.WriteLine("Pomyslnie stworzono nowy plan.");
+    System.Threading.Thread.Sleep(3000);
+    Console.Clear();
+
     return new Plan(name, priority, category);
+}
+bool CheckDateTimeFormat(string dateTime)
+{
+    if (string.IsNullOrEmpty(dateTime) || dateTime.Length < 9)
+    {
+        Console.Clear();
+        Console.WriteLine("BŁĄD! Nie podałeś daty!");
+        System.Threading.Thread.Sleep(3000);
+        Console.Clear();
+
+        return false;
+    }
+    if (dateTime[4] != ' ' && dateTime[4] != '-' && dateTime[4] != '/' && dateTime[4] != '.')
+    {
+        Console.Clear();
+        Console.WriteLine("BŁĄD! Niepoprawny format!");
+        System.Threading.Thread.Sleep(3000);
+        Console.Clear();
+
+        return false;
+    }
+    if (dateTime[7] != ' ' && dateTime[7] != '-' && dateTime[7] != '/' && dateTime[7] != '.')
+    {
+        Console.Clear();
+        Console.WriteLine("BŁĄD! Niepoprawny format");
+        System.Threading.Thread.Sleep(3000);
+        Console.Clear();
+
+        return false;
+    }
+    else return true;
+}
+DateTime PromptDateTime()
+{
+    var day = Convert.ToInt32(Console.ReadLine());
+    var month = Convert.ToInt32(Console.ReadLine());
+    var year = Convert.ToInt32(Console.ReadLine());
+
+    return new DateTime(year, month, day, 0, 0, 0, 0);
+}
+bool CheckPriority(int priority)
+{
+    if (priority < 0 || priority > 10)
+    {
+        Console.Clear();
+        Console.WriteLine("BŁĄD! Podany priorytet wykracza poza skale!");
+        System.Threading.Thread.Sleep(3000);
+        Console.Clear();
+
+        return false;
+    }
+    else return true;
 }
 void EditPlan()
 {
     Console.Clear();
-    Console.WriteLine("Edytujesz plan");
+    Console.WriteLine("Wybierz plan, który chcesz edytować.\n");
+
+    int i = 1;
+    foreach(var plan in PlanList)
+    {
+
+    }
+
     Console.ReadKey();
 }
 void BrowsePlans()
